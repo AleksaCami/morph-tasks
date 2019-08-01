a77bc3dc8870bc0528d708b610df4a1b30333b9b
AleksaCamilovic <aleksa.camilovic@outlook.com>
2019-08-01 13:00:30 +0200

    header nav design

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,12 @@
 <template>
   <header>
-    <nav class="navbar navbar-expand-sm navbar-dark bg-dark mb-5">
+    <nav class="navbar navbar-expand-sm navbar-dark nav-color mb-5">
       <div class="container">
-        <a class="navbar-brand" href="#">
+        <a style="color: #fff" class="navbar-brand" href="#">
           <font-awesome-icon :icon="['fab', 'youtube']" />
-          <span class="ml-2">YouTube</span>
+          <span class="ml-2">YouTube videos</span>
         </a>
-        <router-link to="/login" @click.native="logout()" replace>Logout</router-link>
+        <router-link style="color: #fff" to="/login" @click.native="logout()" replace>Logout</router-link>
       </div>
     </nav>
   </header>
@@ -24,4 +24,14 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+$white: #fff;
+$accent: #2196f3;
+
+.nav-text {
+    color: $white
+}
+.nav-color {
+    background: $accent
+}
+
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,6 @@
 <template>
   <div class="home">
     <Header/>
-    <!-- <SearchForm v-on:search="search"/>
-    <SearchResults
-      v-if="videos.length > 0"
-      v-bind:videos="videos"
-      v-bind:reformattedSearchString="reformattedSearchString"
-    />
-    <Pagination
-      v-if="videos.length > 0"
-      v-bind:prevPageToken="api.prevPageToken"
-      v-bind:nextPageToken="api.nextPageToken"
-      v-on:prev-page="prevPage"
-      v-on:next-page="nextPage"
-    /> -->
   </div>
 </template>
 
